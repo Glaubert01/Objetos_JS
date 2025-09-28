2178b1cf84381224a826f2f8d1abfa9e751c6c70
Glaubert Nunes da Rosa <124006122+Glaubert01@users.noreply.github.com>
2025-09-28 19:58:33 -0300

    update: entendendo sobre objetos

---
+++ objeto.js
@@ -0,0 +1,10 @@
+const listaCPF = ["123.456.789-00", "987.654.321-00", "111.222.333-44"];
+
+const infosPessoa = {
+  nome: "João Silva",
+  idade: 30,
+  cpf: "123.456.789-00",
+  turma: "JavaScript",
+};
+console.log("Lista de CPFs:", listaCPF);
+console.log("Informações da Pessoa:", infosPessoa);
