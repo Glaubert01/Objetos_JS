bf192d394dc204c88ad563f1c5211101a6f2d535
Glaubert Nunes da Rosa <124006122+Glaubert01@users.noreply.github.com>
2025-09-28 20:16:21 -0300

    update: manipulando os dados de um objeto

---
+++ alterando-objeto.js
@@ -0,0 +1,10 @@
+const estudante = {
+  nome: "João Silva",
+  idade: 30,
+  cpf: "123.456.789-00",
+  turma: "JavaScript",
+};
+
+estudante.telefone = "1199999-9999";
+console.log(estudante.telefone);
+console.log(estudante);
