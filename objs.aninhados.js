d305a2cca0c1f7f1562f8b72f1dcb49e7092bb55
Glaubert Nunes da Rosa <124006122+Glaubert01@users.noreply.github.com>
2025-09-28 21:05:58 -0300

    update: inserindo objetos aninhados

---
+++ objs.aninhados.js
@@ -0,0 +1,18 @@
+const estudante = {
+  nome: "João Silva",
+  idade: 30,
+  cpf: "123.456.789-00",
+  turma: "JavaScript",
+  bolsista: true,
+  telefones: ["1199999-9999", "1198888-8888"],
+};
+
+estudante.endereco = {
+  rua: "Rua dos pinheiros",
+  numero: 1293,
+  bairro: "Centro",
+  cidade: "São Paulo",
+  estado: "SP",
+};
+console.log(estudante);
+console.log(estudante.endereco.rua);
