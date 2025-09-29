cf9287c25c08d05da22fd1fdef32345ffef79d9b
Glaubert Nunes da Rosa <124006122+Glaubert01@users.noreply.github.com>
2025-09-28 21:00:00 -0300

    update: inserindo arrays no objeto

---
+++ dados-valores.js
@@ -0,0 +1,12 @@
+const estudante = {
+  nome: "João Silva",
+  idade: 30,
+  cpf: "123.456.789-00",
+  turma: "JavaScript",
+  bolsista: true,
+  telefones: ["1199999-9999", "1198888-8888"],
+};
+
+console.log(estudante.telefones[0]);
+console.log(estudante.telefones[1]);
+console.log(estudante);
