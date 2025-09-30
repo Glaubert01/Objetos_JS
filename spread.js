f68dfa509c4cd45a1da981e252c4ffc9c4e305e6
Glaubert Nunes da Rosa <124006122+Glaubert01@users.noreply.github.com>
2025-09-29 21:43:10 -0300

    update: iserindo dados de objetos no spread

---
+++ spread.js
@@ -0,0 +1,26 @@
+const estudante = {
+  nome: "João Silva",
+  idade: 30,
+  cpf: "123.456.789-00",
+  turma: "JavaScript",
+  bolsista: true,
+  telefones: ["1199999-9999", "1198888-8888"],
+  enderecos: [
+    {
+      rua: "Rua dos pinheiros",
+      numero: 1293,
+      bairro: "Centro",
+      cidade: "São Paulo",
+      estado: "SP",
+      complemento: "ap 934",
+    },
+    {
+      rua: "Avenida dos estados",
+      numero: 5000,
+      bairro: "Bela Vista",
+      cidade: "São Paulo",
+      estado: "SP",
+      complemento: null,
+    },
+  ],
+};
